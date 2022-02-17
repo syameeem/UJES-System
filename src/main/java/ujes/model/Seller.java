af19445f79d604606051ac4b7c63166bdccd988f
11318903 <75016327+11318903@users.noreply.github.com>
2022-02-17 15:28:18 +0800

    Update Seller.java

--- src/main/java/ujes/model/Seller.java
+++ src/main/java/ujes/model/Seller.java
@@ -7,7 +7,7 @@ public class Seller {
 	private String spassw;
 	private String shopName;
 	private String sEmail;
-	private int accNo;
+	private String accNo;
 	private int aID;
 	private boolean valid;
 	
@@ -26,8 +26,8 @@ public class Seller {
 	public void setSEmail(String sEmail) {this.sEmail = sEmail;}
 	public String getSEmail() {return sEmail;}
 	
-	public void setAccNo(int accNo) {this.accNo = accNo;}
-	public int getAccNo() {return accNo;}
+	public void setAccNo(String accNo) {this.accNo = accNo;}
+	public String getAccNo() {return accNo;}
 	
 	public void setAID(int aID) {this.aID = aID;}
 	public int getAID() {return aID;}
