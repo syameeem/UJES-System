9e89e45be64532de11da23d55cdbe92b8b2f1a77
11318903 <75016327+11318903@users.noreply.github.com>
2022-02-21 03:23:53 +0800

    Update LoginBuyer.java

--- src/main/java/ujes/controller/LoginBuyer.java
+++ src/main/java/ujes/controller/LoginBuyer.java
@@ -11,7 +11,7 @@ import ujes.model.Buyer;
 /**
  * Servlet implementation class LoginLogoutController
  */
-@WebServlet("/LoginBuyer")
+@WebServlet("/this_LoginBuyer")
 public class LoginBuyer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private BuyerDAO dao;
