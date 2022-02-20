976dfefa5135c716ff91895536cd604c896ff655
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-20 20:55:40 +0800

    Update AddCategory.java

--- src/main/java/ujes/controller/AddCategory.java
+++ src/main/java/ujes/controller/AddCategory.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +15,6 @@ import javax.servlet.http.Part;
 import ujes.dao.CategoryDAO;
 import ujes.model.Category;
 
-@MultipartConfig
 @WebServlet("/AddCategory")
 public class AddCategory extends HttpServlet {
 	private static final long serialVersionUID = 1L;
