b825572f9dec1c6f929e2220263d78246a58b1c5
Afarrhana_ <91364758+Afarrhana@users.noreply.github.com>
2022-02-20 21:06:48 +0800

    Update ConnectionManager.java

--- src/main/java/ujes/db/ConnectionManager.java
+++ src/main/java/ujes/db/ConnectionManager.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 public class ConnectionManager {
 	static Connection con;
 	private static final String DB_DRIVER= "org.postgresql.Driver";
-	private static final String DB_CONNECTION= "jdbc:postgres://qzwklakdeifcap:fd2e800c27fb0a95959d3f7955ddb5a52b0508dcd9ed2f7fa3e9c5ddf70517b7@ec2-54-198-213-75.compute-1.amazonaws.com:5432/dbbdc6o3astnk";
-	private static final String DB_USER= "qzwklakdeifcap";
-	private static final String DB_PASSWORD= "fd2e800c27fb0a95959d3f7955ddb5a52b0508dcd9ed2f7fa3e9c5ddf70517b7";
+	private static final String DB_CONNECTION= "jdbc:postgres://ihawdciqkgdspx:d0a32ddb478ecf1390eb0b275d3ed8278804864c252d4b8c6eb2faebd733a514@ec2-54-209-221-231.compute-1.amazonaws.com:5432/de2ejfdl93uqof";
+	private static final String DB_USER= "ihawdciqkgdspx";
+	private static final String DB_PASSWORD= "d0a32ddb478ecf1390eb0b275d3ed8278804864c252d4b8c6eb2faebd733a514";
 	
 	public static Connection getConnection() {
 		// TODO Auto-generated method stub
