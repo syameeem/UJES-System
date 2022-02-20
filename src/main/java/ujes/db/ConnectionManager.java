a439be1ce859bf9ed2e3d8608d880a24018f5d5c
nadiaaida <94579202+nadiaaida@users.noreply.github.com>
2022-02-21 01:34:40 +0800

    Update ConnectionManager.java

--- src/main/java/ujes/db/ConnectionManager.java
+++ src/main/java/ujes/db/ConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
 	static Connection con;
 	private static final String DB_DRIVER= "org.postgresql.Driver";
-	private static final String DB_CONNECTION= "jdbc:postgres://ec2-54-209-221-231.compute-1.amazonaws.com:5432/de2ejfdl93uqof";
+	private static final String DB_CONNECTION= "jdbc:postgres://ec2-54-209-221-231.compute-1.amazonaws.com:5432/de2ejfdl93uqof" + "/de2ejfdl93uqof";
 	private static final String DB_USER= "ihawdciqkgdspx";
 	private static final String DB_PASSWORD= "d0a32ddb478ecf1390eb0b275d3ed8278804864c252d4b8c6eb2faebd733a514";
 	
